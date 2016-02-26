8e058c22dc8000e0647aadddeeb772ca85d81c40
Nafisa <nafisaisrail@gmail.com>
2016-02-26 15:54:06 +0600

    lesson 5

--- lesson3.2.py
+++
@@ -1,9 +0,0 @@
-def sep(i,x):
-    for i in x:
-        print i,'-', len(i)
-
-y = list(map(str,raw_input("Enter some words\n").split()))
-i=0
-sep(i,y)
-
-raw_input()
---
+++ lesson5.1.py
@@ -0,0 +1,31 @@
+class Number():
+    x = int(raw_input('Enter number from 1 to 9\n'))
+    
+    def __init__(self):
+        self.x = self.x
+
+    def mult(self):
+        n1 = str(input("Enter several numbers\n"))
+        n2 = int(input("How many times repeat?\n"))
+        return(n1*n2)
+
+    def power(self):
+        n1 = input("Raising to the power\n")
+        return(self.x ** n1)
+
+    def loop(self,x,z):
+        z = [x for x in range(x, x + 10)]
+
+f = Number()
+if f.x in range(1, 4):
+    print(f.mult())
+elif f.x in range(3, 8):
+    print(f.power())
+elif f.x in range(7, 10):
+    print(f.loop())
+else:
+    print("Error")
+
+
+raw_input()
+
