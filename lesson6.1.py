8439e15862f3d48a23796224b4a50f4b2d67f8dc
Nafisa <nafisaisrail@gmail.com>
2016-02-26 15:56:44 +0600

    lesson 6

--- lesson6.1.py
+++ lesson6.1.py
@@ -1,4 +1,11 @@
-class Number():
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+class A(object):
+    __metaclass__=ABCMeta
+    x = 0   
+
+class Number(object):
     x = int(raw_input('Enter number from 1 to 9\n'))
     
     def __init__(self):
@@ -15,8 +22,15 @@ class Number():
 
     def loop(self,x,z):
         z = [x for x in range(x, x + 10)]
+        return(x)
 
-f = Number()
+class Ch(Number):
+    def __init__(self):
+        self.x = self.x
+        pass
+        super(Ch,self).__init__()
+
+f = Ch()
 if f.x in range(1, 4):
     print(f.mult())
 elif f.x in range(3, 8):
@@ -28,4 +42,3 @@ else:
 
 
 raw_input()
-
